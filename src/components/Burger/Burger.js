027df31be236c70c2d01fcd56351a9e71976b493
klein20 <olexandrkleyn@gmail.com>
2020-02-05 14:12:57 +0200

    made orderbtn and modal window

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,7 +7,7 @@ const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient ley={igKey + i} type={igKey} />
+                return <BurgerIngredient key={igKey + i} type={igKey} />
             });
         })
         .reduce((arr, el) => {
