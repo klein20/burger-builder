78964178d0a821e412cb505d56da51c652a93552
klein20 <olexandrkleyn@gmail.com>
2020-02-23 16:37:07 +0200

    user sees only his own orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -24,7 +24,7 @@ export const purchaseBurgerStart = () => {
 
 export const purchaseBurger = ( orderData, token ) => {
     return dispatch => {
-        dispatch( purchaseBurgerStart() );
+        dispatch(purchaseBurgerStart());
         axios.post( '/orders.json?auth=' + token, orderData )
             .then( response => {
                 console.log( response.data );
@@ -62,10 +62,11 @@ export const fetchOrdersStart = () => {
     };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        axios.get( '/orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equilTo="' + userId + '"';
+        axios.get( '/orders.json?auth=' + queryParams)
             .then( res => {
                 const fetchedOrders = [];
                 for ( let key in res.data ) {
