66b4777a0169d2c95faef79446deb4cb6ff9b24a
klein20 <olexandrkleyn@gmail.com>
2020-03-02 18:18:10 +0200

    OrderSummary, BurgerIngredient, Modal converted and added React.memo

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,27 +4,25 @@ import classes from './Modal.css';
 import Auxuliary from '../../../hoc/Auxliliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
+const modal = props => {
 
-    shouldComponentUpdate ( nextProps, nextState ) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
+    // shouldComponentUpdate ( nextProps, nextState ) {
+    //     return nextProps.show !== props.show || nextProps.children !== props.children;
+    // }
 
-    render () {
         return (
             <Auxuliary>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <Backdrop show={props.show} clicked={props.modalClosed} />
                 <div
                     className={classes.Modal}
                     style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
+                        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: props.show ? '1' : '0'
                     }}>
-                    {this.props.children}
+                    {props.children}
                 </div>
             </Auxuliary>
         )
-    }
 }
 
-export default Modal;
+export default React.memo(modal, (prevProps, nextProps) => nextProps.show == prevProps.show && nextProps.children == prevProps.children);
